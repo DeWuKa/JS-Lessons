a072f2ff57a5950e35328ddd509b9e2890f612bb
Damian <damian.krzem@gmail.com>
2018-04-20 08:16:34 +0200

    up

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,6 +1,28 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1990);
+
+function calculateAge(year){
+    console.log(2016-year);
+}
+
+
+var retirement = function(year){
+    console.log(65-(2016-year));
+}
+
+console.log(age);
+var age =23;
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
+
+
 ///////////////////////////////////////
 // Lecture: Scoping
 
