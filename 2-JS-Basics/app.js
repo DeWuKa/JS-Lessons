a971ef937a1b7fece39e572589786592bf2ba2fa
Damian <damian.krzem@gmail.com>
2018-05-22 14:27:32 +0200

    up

--- 2-JS-Basics/app.js
+++ 2-JS-Basics/app.js
@@ -9,24 +9,29 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", function(){
-    var dice = Math.floor(Math.random()*6+1);
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
-    diceDOM.src = "dice-" + dice + ".png";    
 
-    if (dice != 1) {
-        roundScore += dice;
-        document.querySelector("#current-" + activePlayer).textContent = roundScore;
-    } else {
-        nextPlayer();
+    if(gamePlaying){
+        var dice = Math.floor(Math.random()*6+1);
+        var diceDOM = document.querySelector(".dice");
+        diceDOM.style.display = "block";
+        diceDOM.src = "dice-" + dice + ".png";    
+    
+        if (dice != 1) {
+            roundScore += dice;
+            document.querySelector("#current-" + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
     }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function(){
+   if(gamePlaying){
+   
     scores[activePlayer] += roundScore;
     document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
 
@@ -35,9 +40,11 @@ document.querySelector(".btn-hold").addEventListener("click", function(){
         document.querySelector(".dice").style.display = "none";
         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
         document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        gamePlaying = false;
     } else {
         nextPlayer();
     }
+}
 });
 
 function nextPlayer(){
@@ -59,6 +66,7 @@ function init(){
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
     
     document.querySelector(".dice").style.display = "none";
     document.getElementById("score-0").textContent = "0";
@@ -71,6 +79,5 @@ function init(){
     document.querySelector(".player-1-panel").classList.remove("winner");
     document.querySelector(".player-0-panel").classList.remove("active");   
     document.querySelector(".player-1-panel").classList.remove("active");
-    document.querySelector(".player-0-panel").classList.add("active");
-    
+    document.querySelector(".player-0-panel").classList.add("active");    
 }
