7148e59d3bfcdf859c17650be67f4c80aac10c7d
Damian <damian.krzem@gmail.com>
2018-09-27 19:46:15 +0200

    up

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,24 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function(){
+
+   
+})();
+
+
+//UI CONTROLER
+var UIController = (function(){
+
+    // Some code
+    
+})();
+
+
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+
+   
+
+})(budgetController, UIController);
