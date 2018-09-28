43cb94866806d30a484cf7334263943e9324a1b3
Damian <damian.krzem@gmail.com>
2018-09-28 07:40:52 +0200

    uo

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -6,10 +6,28 @@ var budgetController = (function(){
 })();
 
 
-//UI CONTROLER
+//UI CONTROLLER
 var UIController = (function(){
+ 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    };
 
-    // Some code
+    return {
+        getinput: function(){
+                return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOmstrings: function(){
+            return DOMstrings;
+        }
+    };
     
 })();
 
@@ -17,21 +35,35 @@ var UIController = (function(){
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+var setupEventListeners = function(){
+
+    var DOM =UICtrl.getDOmstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event){
+     if (event.keyCode === 13) {
+         ctrlAddItem();
+     }
+ 
+    });
+};
 
 var ctrlAddItem = function(){
      // 1. Get the filed input data
+     var input = UICtrl.getinput();
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    console.log("perkele");
-}    
-document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+};
 
-   document.addEventListener("keypress", function(event){
-    if (event.keyCode === 13) {
-        ctrlAddItem();
+return {
+    init: function(){
+        console.log("app has started");
+        setupEventListeners();
     }
-   });
-
+};
 })(budgetController, UIController);
+
+controller.init();
